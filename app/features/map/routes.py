60cff72abda1720208bb6d6ae93d5b6acef862a9
Gautam Mekkat <12224008+Blue9@users.noreply.github.com>
2023-02-01 14:35:38 -0500

    Allow community map

--- app/features/map/routes.py
+++ app/features/map/routes.py
@@ -31,53 +31,15 @@ async def load_map(
         )
         return GetMapResponse(pins=pins)
     else:
-        if request.map_type != "custom":
-            raise HTTPException(403)
-        pins = await map_store.get_featured_users_map(
-            region=request.region, user_ids=request.user_ids or [], categories=request.categories, limit=200
-        )
-        return GetMapResponse(pins=pins)
-
-
-@router.post("/global", response_model=GetMapResponse)
-async def get_global_map(
-    request: DeprecatedGetMapRequest,
-    map_store: MapStore = Depends(get_map_store),
-    _user: InternalUser = Depends(get_caller_user),
-):
-    """Get a map of all users."""
-    pins = await map_store.get_community_map(region=request.region, categories=request.categories)
-    return GetMapResponse(pins=pins)
-
-
-@router.post("/following", response_model=GetMapResponse)
-async def get_following_map(
-    request: DeprecatedGetMapRequest,
-    map_store: MapStore = Depends(get_map_store),
-    user: InternalUser = Depends(get_caller_user),
-):
-    """Get a map of friends."""
-    pins = await map_store.get_friend_map(region=request.region, user_id=user.id, categories=request.categories)
-    return GetMapResponse(pins=pins)
-
-
-@router.post("/saved-posts", response_model=GetMapResponse)
-async def get_saved_posts_map(
-    request: DeprecatedGetMapRequest,
-    map_store: MapStore = Depends(get_map_store),
-    user: InternalUser = Depends(get_caller_user),
-):
-    """Get a map of saved posts."""
-    pins = await map_store.get_saved_posts_map(region=request.region, user_id=user.id, categories=request.categories)
-    return GetMapResponse(pins=pins)
-
-
-@router.post("/custom", response_model=GetMapResponse)
-async def get_custom_map(
-    request: DeprecatedCustomMapRequest,
-    map_store: MapStore = Depends(get_map_store),
-    _user: InternalUser = Depends(get_caller_user),
-):
-    """Get a map of the given users."""
-    pins = await map_store.get_custom_map(region=request.region, user_ids=request.users, categories=request.categories)
-    return GetMapResponse(pins=pins)
+        # Anonymous accounts
+        if request.map_type == "custom":
+            pins = await map_store.get_featured_users_map(
+                region=request.region, user_ids=request.user_ids or [], categories=request.categories, limit=200
+            )
+            return GetMapResponse(pins=pins)
+        elif request.map_type == "community":
+            pins = await map_store.get_guest_community_map(
+                region=request.region, categories=request.categories, limit=200
+            )
+            return GetMapResponse(pins=pins)
+    raise HTTPException(403)
